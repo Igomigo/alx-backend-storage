57ce5057dea2051503ebbf7c7f41deb9026d3ec9
Igomigo <igomigofatai@gmail.com>
2024-01-09 12:22:28 +0000

    returned list of docs

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -4,5 +4,5 @@
 
 def schools_by_topic(mongo_collection, topic):
     """ returns the list of school having a specific topic """
-    documents = mongo_collection.find({topics: "topic"})
+    documents = mongo_collection.find({topics: topic})
     return list(documents)
